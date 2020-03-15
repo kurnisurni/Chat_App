ee0e8c6a81b8b20923bfa4f9b750329c9e91257a
Mantas <gusmantas@gmail.com>
2020-03-15 19:29:22 +0100

    bugs

---
+++ src/main/java/com/example/demo/services/UserChannelsId.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import java.io.Serializable;
+
+public class UserChannelsId implements Serializable {
+
+    private int channelId;
+    private int userId;
+
+    public UserChannelsId() {}
+}
--- src/main/resources/static/views/home.js
+++ src/main/resources/static/views/home.js
@@ -11,5 +11,25 @@ export default{
       <userChannels />
       <friendlist />
     </div>
-    `
+    `,
+    async created(){
+      
+      let users = await fetch('/rest/users')
+      users = await users.json()
+      this.$store.commit('displayUsers', users)
+      console.log('created')
+      console.log(users)
+      
+      let messages = await fetch('/rest/messages')
+      messages = await messages.json()
+      this.$store.commit('displayMessages', messages)
+      console.log(messages)
+
+      let url = 'rest/users/channels/id/' +  this.$store.state.currentUser.id
+      let channels = await fetch(url)
+      channels = await channels.json()
+      this.$store.commit('displayUserChannels', channels)
+      console.log(channels)
+      
+    }
 }
