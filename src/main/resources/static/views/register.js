dee5e04c123ad132fe08719faf565b60649168d8
Kurnia <kurniasuryai@gmail.com>
2020-03-23 15:23:42 +0100

    register worked

---
+++ src/main/resources/static/views/register.js
@@ -0,0 +1,12 @@
+import register from '../components/registerComponent.js'
+
+export default{
+    components:{
+       register
+    },
+    template:`
+    <div>
+      <register />
+    </div>
+    `
+}
