3cddf3f7aca055eca7d8b8ded37fe9f033f49e46
Emanuel Harling <emanuelharling@hotmail.com>
2020-03-30 21:37:36 +0200

    fixed line breaks = shift+enter, send message = enter

--- src/main/resources/static/components/messageInput.js
+++ src/main/resources/static/components/messageInput.js
@@ -9,7 +9,7 @@ export default{
         placeholder="Type your message here..." 
         rows="10" 
         cols="30" 
-         
+        @keydown="inputHandler"
         required></textarea>
         <button>Send</button>
         </form>
@@ -26,31 +26,37 @@ export default{
   },
   
   methods: {
-      async submitMessage(){
-        if (this.messageInput != ''){
-
-          let message;
-          let messageBody = {
-            user_id: this.$store.state.currentUser.id,
-            content: this.messageInput,
-            channel_id: this.currentChannel,
-            message_time: Date.now()
-          }
-          try{
-            message = await fetch('/rest/messages',{
-              method: 'POST',
-              body: JSON.stringify(messageBody),
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            })
-          }catch(e){
-          console.log(e)
-          }
-        }
-
-        this.messageInput = ''
+    inputHandler(e){
+      if (e.keyCode === 13 && !e.shiftKey){
+        e.preventDefault()
+        this.submitMessage()
       }
+    },
+    async submitMessage(){
+      if (this.messageInput != ''){
+
+        let message;
+        let messageBody = {
+          user_id: this.$store.state.currentUser.id,
+          content: this.messageInput,
+          channel_id: this.currentChannel.id,
+          message_time: Date.now()
+        }
+        try{
+          message = await fetch('/rest/messages',{
+            method: 'POST',
+            body: JSON.stringify(messageBody),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+        }catch(e){
+        console.log(e)
+        }
+      }
+
+      this.messageInput = ''
+    }
   },
 
   computed: {
