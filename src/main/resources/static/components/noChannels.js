5be75faf723ef6bf2b18175ee5f2f977602bb8b6
Emanuel Harling <emanuelharling@hotmail.com>
2020-04-01 22:41:24 +0200

    if you leave all channels there is an almost blank page

---
+++ src/main/resources/static/components/noChannels.js
@@ -0,0 +1,8 @@
+export default {
+  template: `
+  <div class="noChannelsDiv">
+    <h3>You have not joined any channels!</h3>
+    <p>Pick a channel to join or create your own!</p>
+  </div>
+  `
+}
